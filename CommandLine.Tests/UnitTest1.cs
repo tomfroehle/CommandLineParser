0bdb828869dc84de5d3b09836e755ed27177ac0d
Thomas Fröhle <Thomas.Froehle@generic.de>
2021-12-16 10:56:39 +0100

    Get back to basics

--- CommandLine.Tests/UnitTest1.cs
+++ CommandLine.Tests/UnitTest1.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Net;
-using System.Text;
 using NUnit.Framework;
 
 namespace CommandLine.Tests
@@ -10,26 +7,18 @@ namespace CommandLine.Tests
         [Test]
         public void ArgsTest()
         {
-            var args = ArgsProvider.Provide("j#,f?,w*,z+,i$,d%,b&",
+            var args = ArgsProvider.Provide("j#,f?,w*",
                 new[]
                 {
                     "-j123",
                     "-f",
-                    "-wHallo",
-                    "-z12,5",
-                    "-i192.168.0.1",
-                    "-d25.03.2019 12:25",
-                    "-bSGFsbG8="
+                    "-wHallo"
                 }
             );
 
             Assert.AreEqual(true, args.Get<bool>("f"));
             Assert.AreEqual(123, args.Get<int?>("j"));
             Assert.AreEqual("Hallo", args.Get<string>("w"));
-            Assert.AreEqual(12.5, args.Get<float?>("z"));
-            Assert.AreEqual(IPAddress.Parse("192.168.0.1"), args.Get<IPAddress>("i"));
-            Assert.AreEqual(new DateTime(2019, 03, 25, 12, 25, 0), args.Get<DateTime?>("d"));
-            Assert.AreEqual(Encoding.UTF8.GetBytes("Hallo"), args.Get<byte[]>("b"));
         }
 
         [TestCase("-f", true)]
